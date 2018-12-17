4b67569c52eae8e10c0d6ddd6eee2de68cc95611
Fergus Lemon <fergus.lemon@gmail.com>
2018-12-17 20:15:55 +0000

    Refactor solution - option 2, cleaner code with less lines and nicer readability.

--- src/fizzBuzz.js
+++ src/fizzBuzz.js
@@ -4,14 +4,8 @@ function JavascriptBuzz() {
 JavascriptBuzz.prototype.play = function(number) {
   let result = "";
 
-  if (number%3 === 0) {
-    result += "Javascript";
-    }
-    if (number%5 === 0) {
-      result += "Buzz";
-    }
-    if (result.length === 0 || number === 0) {
-      return number;
-    }
+  if (number%3 === 0) result += "Javascript";
+  if (number%5 === 0) result += "Buzz";
+  if (result.length === 0 || number === 0) return number;
   return result;
 };
